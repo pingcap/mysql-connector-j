63047053603fd0704545d392ea3270e448953d86
lastincisor <64511824+lastincisor@users.noreply.github.com>
2022-06-09 16:09:16 +0800

    prepare bug fix (#2)
    Co-authored-by: song <yongzhong.song@pingcap.com>

--- src/main/core-impl/java/com/mysql/cj/ServerPreparedQuery.java
+++ src/main/core-impl/java/com/mysql/cj/ServerPreparedQuery.java
@@ -150,17 +150,19 @@ public class ServerPreparedQuery extends ClientPreparedQuery {
             boolean checkEOF = !this.session.getServerSession().isEOFDeprecated();
 
             if (this.parameterCount > 0) {
+                this.parameterFields = this.session.getProtocol().read(ColumnDefinition.class, new ColumnDefinitionFactory(this.parameterCount, null))
+                        .getFields();
                 if (checkEOF) { // Skip the following EOF packet.
                     this.session.getProtocol().skipPacket();
                 }
-
-                this.parameterFields = this.session.getProtocol().read(ColumnDefinition.class, new ColumnDefinitionFactory(this.parameterCount, null))
-                        .getFields();
             }
 
             // Read in the result set column information
             if (fieldCount > 0) {
                 this.resultFields = this.session.getProtocol().read(ColumnDefinition.class, new ColumnDefinitionFactory(fieldCount, null));
+                if (checkEOF) { // Skip the following EOF packet.
+                    this.session.getProtocol().skipPacket();
+                }
             }
         }
     }
